845a37c3e48a126534f19f7d9fc47cb67b44cc21
ieatricealot <panit935@gmail.com>
2025-09-08 19:02:44 -0400

    Update 67something.py
    newwwwe

--- 67something.py
+++ 67something.py
@@ -1,9 +1,23 @@
-import tkinter as tk
+import tkinter as tk 
+import time as tm
 class Timer :
     def __init__(self,root):
         self.root=root
         self.root.title("Timer")
+        self.start = tk.Button(root, fg="black", bg="green" , text="Start")
+        self.start.pack(pady=20)
+
+        self.start = tk.Button(root, fg="black", bg="red" , text="Hello", justify=['left'])
+        self.start.pack(pady=20)
+
+       #67
+# self.start = tk.wraplength:ScreenUnits = 0self.start.pack(pady=67)
+
+
+
+
 if __name__=="__main__":
     root = tk.Tk()
     timer=Timer(root)
     root.mainloop()
+
